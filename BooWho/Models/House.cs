60e3d0672ded49fcdf1e93d31878ed8878e4bf8e
Sarah Beth Langley <sarahbethdoescode@gmail.com>
2023-02-02 11:10:39 -0600

    full functionality for SQL data, created models and a respository as well

--- BooWho/Models/House.cs
+++ BooWho/Models/House.cs
@@ -2,5 +2,9 @@
 {
     public class House
     {
+        public int Id { get; set; }
+        public int UserProfileId { get; set; }
+        public string Address { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
